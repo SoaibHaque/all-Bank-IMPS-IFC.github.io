8cd9dc18c28c75f8cd8d9035e617a122fdf65fce
Sekh Soaib Haque <72245160+SoaibHaque@users.noreply.github.com>
2021-10-13 18:36:28 +0530

    create a ifc search site

---
+++ index.js
@@ -0,0 +1,42 @@
+const Selector = ele => document.querySelector(ele),
+    SelectorAll = eleAll => document.querySelectorAll(eleAll),
+    newEle = newele => document.createElement(newele),
+    hrefIndex = href => window.location.href.indexOf(href),
+    lsGet = item => localStorage.getItem(item),
+    lsSet = (key, value) => localStorage.setItem(key, value),
+    lsRem = item => localStorage.removeItem(item),
+    sI = (fun, time) => setInterval(fun, time),
+    sT = (fun, time) => setTimeout(fun, time),
+    cI = intervaliable => clearInterval(intervaliable),
+    cT = intervaliable => clearTimeout(intervaliable),
+    docLis = (eve, fun) => document.addEventListener(eve, fun);
+
+// changes occur after select bank
+
+let whatsAppSend = () => {
+    let url = `http://api.whatsapp.com/send?phone=919818855510&text=${encodeURI(Selector('#ifc').value)}`;
+    window.open(url, '_blank');
+}
+
+let copyIFC = () => {
+    Selector('#ifc').select();
+    document.execCommand("copy");
+}
+
+let ifcCode = (v) => {
+    Selector('#ifc').value = v;
+}
+
+let afterSelect = (v) => {
+    let value = v.split("_");
+    Selector("#search").value = value[0];
+    ifcCode(value[1]);
+}
+
+Selector('#search').onchange = () => afterSelect(Selector('#search').value);
+
+Selector('#search').onfocus = () => Selector('#search').select();
+
+Selector('#ifc').onfocus = () => copyIFC();
+
+Selector('#send').onclick = () => Selector('#ifc').value !== '' ? whatsAppSend() : undefined;
